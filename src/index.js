a5f7c742c665e418d65be9ff918ea5d6577cdb80
Vatsal Sood <vatsalsood@dal.ca>
2020-09-21 15:30:55 -0300

    Setup files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+
+// Relative Imports
+import { getRows, getColumns } from "./processapi";
+import { useStyles } from "./style/styles";
+
+// Absolute Imports
+import Table from "@material-ui/core/Table";
+import TableContainer from "@material-ui/core/TableContainer";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+// import TeamTablePagination from "./components/TeamTablePagination";
+
+const App = () => {
+  const [rows, setRows] = useState([]);
+  const [filteredRows, setFilteredRows] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    console.log();
+  });
+
+  /**
+   * This function searches the entries with the text typed into the input field
+   * @param {event} event
+   */
+  const filterRows = (event) => {
+    let searchText = event.target.value.toLowerCase();
+    setFilteredRows(
+      rows.filter((row) => row.name.toLowerCase().includes(searchText))
+    );
+  };
+  return (
+    <div className={classes.root}>
+      <Grid container justify="center" spacing={3}>
+        {/**
+         * This is the search field
+         * Could add validation- no invalid characters allowed etc.
+         */}
+        <Grid item xs={6}>
+          <TextField
+            id="standard-basic"
+            label="Search by name"
+            fullWidth
+            // onChange={(e) => {
+            //   filterRows(e);
+            // }}
+          />
+        </Grid>
+
+        {/* <Grid item xs={12}>
+          <TeamTablePagination
+            filteredRows={filteredRows}
+            page={page}
+            rowsPerPage={rowsPerPage}
+            setPage={setPage}
+            setRowsPerPage={setRowsPerPage}
+          />
+        </Grid> */}
+      </Grid>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
